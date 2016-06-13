bd63b51cbda78a50b1d00bf950476075644b64c8
Matt Soria <matt.m.soria@gmail.com>
2016-06-13 17:11:33 -0500

    Adding locations details

--- assets/js/build/site.js
+++ assets/js/build/site.js
@@ -1005,59 +1005,77 @@ var Main = (function($) {
       partnerLat: 41.7837192,
       partnerLng: -87.6325996,
       partnerLocations: [
-        ['Greater Auburn-Gresham Development Corporation', 'undefined', '+1 773-483-3696', 'undefined', 'http://www.gagdc.org/', 41.7503217, -87.65356170000001, 'undefined'],
-        ['Centers For New Horizons Inc', 'undefined', '+1 773-373-5700', 'undefined', 'http://cnh.org/', 41.8186204, -87.61735729999998, 'undefined'],
-        ['Claretian Associate', 'undefined', 'undefined', 'undefined', 'undefined', 41.7296735, -87.54732000000001, 'undefined'],
-        ['Hyde Park Neighborhood Club', 'undefined', 'undefined', 'undefined', 'undefined', 41.7958986, -87.59401860000003, 'undefined'],
-        ['Demoiselle 2 Femme', 'undefined', 'undefined', 'undefined', 'undefined', 41.7224835, -87.6817249, 'undefined'],
-        ['Sinai Community Institute', 'undefined', 'undefined', 'undefined', 'undefined', 41.8621073, -87.6927976, 'undefined'],
-        ['Near West Side Community Development Corporation', 'undefined', 'undefined', 'undefined', 'undefined', 41.8781545, -87.67924619999997, 'undefined'],
-        ['Enlace Chicago', 'undefined', 'undefined', 'undefined', 'undefined', -87.7234694, 41.8407009, 'undefined'],
-        ['BUILD Chicago', 'undefined', 'undefined', 'undefined', 'undefined', -87.75370459999999, 41.8731717, 'undefined']
+        ['Greater Auburn-Gresham Development Corporation', '1159 West 79th Street Chicago, IL', '773-483-3696', 'undefined', 'http://www.gagdc.org/', 41.7503217, -87.65356170000001],
+        ['Centers For New Horizons Inc', 'undefined', '773-373-5700', 'undefined', 'http://cnh.org', 41.8186204, -87.61735729999998],
+        ['Claretian Associate', '9108 South Brandon Avenue, Chicago, IL', '773-734-9181', 'portera@claretianassociates.org', 'https://www.claretianassociates.org', 41.7296735, -87.54732000000001],
+        ['Hyde Park Neighborhood Club', '5480 S. KENWOOD AVENUE, CHICAGO, IL 60615', '773-643-4062', 'info@hpnclub.org', 'http://hpnclub.org', 41.7958986, -87.59401860000003],
+        ['Demoiselle 2 Femme', 'undefined', '773-660-1677', 'undefined', 'http://demoiselle2femme.org', 41.7224835, -87.6817249],
+        ['Sinai Community Institute', '2653 West Ogden Avenue, Chicago, Illinois 60608', '773-257-6508', 'undefined', 'http://www.sinai.org/content/sinai-community-institute-0', 41.8621073, -87.6927976],
+        ['Near West Side Community Development Corporation', '216 South Hoyne Avenue, Chicago, IL 60612', '312-738-2280', 'undefined', 'http://nearwestsidecdc.org/', 41.8781545, -87.67924619999997],
+        ['Enlace Chicago', '773-542-9233', 'info@enlacechicago.org', 'http://www.enlacechicago.org/', -87.7234694, 41.8407009],
+        ['BUILD Chicago', '5100 W. Harrison, Chicago IL 60644', '773-227-2880', 'undefined', 'http://www.buildchicago.org/', -87.75370459999999, 41.8731717]
       ],
-      elementID: 'chicago-partners'
+      elementID: 'chicago-partners',
+      partnerZoom: 11
     };
 
     var nyc = {
       partnerLat: 40.7993063,
       partnerLng: -73.937144,
       partnerLocations: [
-        ['Mount Sinai Adolescent Health Center', 'undefined', 'undefined', 'undefined', 'undefined', 40.7823722, -73.94719570000001, 'undefined'],['Claremont Healthy Village', 'undefined', 'undefined', 'undefined', 'undefined', 40.8313933, -73.90273730000001, 'undefined'],['Urban Health Plan, Inc', 'undefined', 'undefined', 'undefined', 'undefined', 40.825011, -73.89228300000002, 'undefined'],['Two Bridges Neighborhood Council', 'undefined', 'undefined', 'undefined', 'undefined', 40.71093169999999, -73.98848079999999, 'undefined']
+        ['Mount Sinai Adolescent Health Center', '312-320 East 94th Street, New York, NY 10128', '212-423-3000', 'undefined', 'http://www.mountsinai.org/patient-care/service-areas/adolescent-health-center', 40.7823722, -73.94719570000001],
+        ['Claremont Healthy Village', '1276 Fulton Ave., Bronx, NY 10456', ' 718-901-8297', 'undefined', 'http://www.blhfamilymed.com/Community-Involvement/CHVI', 40.8313933, -73.90273730000001],
+        ['Urban Health Plan, Inc', '1065 Southern Boulevard, Bronx, New York 10459', '718-589-2440', 'info@urbanhealthplan.org', 'http://www.urbanhealthplan.org/', 40.825011, -73.89228300000002],
+        ['Two Bridges Neighborhood Council', '275 Cherry Street, New York, NY 10002', '212-566-2729  ', 'info@twobridges.org', 'http://www.twobridges.org/', 40.71093169999999, -73.98848079999999]
       ],
-      elementID: 'nyc-partners'
+      elementID: 'nyc-partners',
+      partnerZoom: 11
     };
 
     var niagara = {
       partnerLat: 43.1132875,
       partnerLng: -79.0355104,
       partnerLocations: [
-        ['Planned Parenthood of Central and Western New York', 'undefined', 'undefined', 'undefined', 'undefined', 42.940527, -78.83722399999999, 'undefined'],['Niagara Employment &amp; Training', 'undefined', 'undefined', 'undefined', 'undefined', 43.09876599999999, -79.04935499999999, 'undefined'],['Community Health Center of Niagara Falls', 'undefined', 'undefined', 'undefined', 'undefined', 43.11303599999999, -79.048565, 'undefined'],['Mount Saint Marys Hospital', 'undefined', 'undefined', 'undefined', 'undefined', 43.15394819999999, -79.03311889999998, 'undefined'],['Niagara Falls Memorial Medical Center', 'undefined', 'undefined', 'undefined', 'undefined', 43.09383820000001, -79.05033229999998, 'undefined'],['Niagara University', 'undefined', 'undefined', 'undefined', 'undefined', 43.1383605, -79.03766719999999, 'undefined']
+        ['Creat a Healthier Niagara Falls Collaborative', '1385 Broadway, 23rd Floor, New York, NY 10018', '212-664-7656', 'undefined', 'http://nyshealthfoundation.org/our-grantees/grantee-profile/create-a-healthier-niagara-falls-collaborative', 40.7528622, -73.987863],
+        ['Planned Parenthood of Central and Western New York', '2697 Main Street, Buffalo 14214', '716-831-2200', 'undefined', 'https://www.plannedparenthood.org/planned-parenthood-central-western-new-york', 42.940527, -78.83722399999999],
+        ['Niagara Employment &amp; Training', '1001 Eleventh Street, Niagara Falls, NY 14301', '716.278.8108', 'undefined', 'http://worksource1.com/', 43.09876599999999, -79.04935499999999],
+        ['Community Health Center of Niagara Falls', '2715 Highland Avenue, Niagara Falls, New York 14305', '716-278-4418', 'undefined', 'http://www.chcb.net/outreach/read.php?id=19', 43.11303599999999, -79.048565],
+        ['Mount Saint Marys Hospital', '3101 Ninth Street, Niagara Falls, NY 14305', '716-284-8917', 'undefined', 'http://www.chsbuffalo.org/Facilities/PrimaryCare/MountStMarysNeighborhoodHealthCenter', 43.1158483, -79.0525589],
+        ['Niagara Falls Memorial Medical Center', '621 Tenth Street, Niagara Falls, NY 14302', '716-278-4000', 'undefined', 'http://nfmmc.org/', 43.09383820000001, -79.05033229999998],
+        ['Niagara University', 'Niagara University, NY 14109', '800-462-2111', 'undefined', 'http://www.niagara.edu/', 43.1383605, -79.03766719999999]
       ],
-      elementID: 'niagara-partners'
+      elementID: 'niagara-partners',
+      partnerZoom: 12
     };
 
     var nashEdgecombe = {
       partnerLat: 35.8893318,
       partnerLng: -77.9239492,
       partnerLocations: [
-        ['Resourceful Communities', 'undefined', 'undefined', 'undefined', 'undefined', 38.8938808, -77.0715055, 'undefined'],['UNC Center for Health Equity Research', 'undefined', 'undefined', 'undefined', 'undefined', 35.9049122, -79.0469134, 'undefined'],['Project Momentum', 'undefined', 'undefined', 'undefined', 'undefined', 35.9419921, -77.79512299999999, 'undefined']
+        ['Resourceful Communities', '1655 N. Fort Myer Drive, Suite 1300, Arlington, Virginia 22209', '703-525-6300', 'webmaster@conservationfund.org', 'http://www.conservationfund.org/what-we-do/resourceful-communities', 38.8938808, -77.0715055],
+        ['UNC Center for Health Equity Research', '323 MacNider Hall, Campus Box 7240, 333 South Columbia Street, Chapel Hill, NC 27599-7240', '919.843.8271', 'cher@unc.edu', 'http://cher.unc.edu/', 35.9049122, -79.0469134],
+        ['Project Momentum', 'undefined', 'undefined', 'undefined', 35.9419921, -77.79512299999999]
       ],
-      elementID: 'nash-edgecombe-partners'
+      elementID: 'nash-edgecombe-partners',
+      partnerZoom: 9
     };
 
     partnerMaps = [chicago, nyc, niagara, nashEdgecombe];
  
 
     $.each(partnerMaps, function(i) { 
-     console.log(this.partnerLat, this.partnerLng, this.elementID, this.partnerLocations);
-     google.maps.event.addDomListener(window, 'load', init(this.partnerLat, this.partnerLng, this.elementID, this.partnerLocations));
+     google.maps.event.addDomListener(window, 'load', init(this.partnerLat, this.partnerLng, this.elementID, this.partnerZoom, this.partnerLocations));
     });
 
     var map;
-    function init(partnerLat, partnerLng, id, partnerLocations) {
+    var infowindow = new google.maps.InfoWindow({
+      content: 'content!',
+      maxWidth: 260
+    });
+    function init(partnerLat, partnerLng, id, partnerZoom, partnerLocations) {
       var mapOptions = {
           center: new google.maps.LatLng(partnerLat, partnerLng),
-          zoom: 11,
+          zoom: partnerZoom,
           zoomControl: true,
           zoomControlOptions: {
               style: google.maps.ZoomControlStyle.DEFAULT,
@@ -1080,23 +1098,35 @@ var Main = (function($) {
       var map = new google.maps.Map(mapElement, mapOptions);
       var locations = partnerLocations;
       for (i = 0; i < locations.length; i++) {
-        if (locations[i][1] =='undefined'){ description ='';} else { description = locations[i][1];}
-        if (locations[i][2] =='undefined'){ telephone ='';} else { telephone = locations[i][2];}
-        if (locations[i][3] =='undefined'){ email ='';} else { email = locations[i][3];}
-        if (locations[i][4] =='undefined'){ web ='';} else { web = locations[i][4];}
-        if (locations[i][7] =='undefined'){ markericon ='/assets/images/pin-sm.png';} else { markericon = locations[i][7];}
+        // if (locations[i][1] == 'undefined'){ description ='';} else { description = locations[i][1];}
+        if (locations[i][1] == 'undefined'){ address ='';} else { address = locations[i][1];}
+        if (locations[i][2] == 'undefined'){ telephone ='';} else { telephone = locations[i][2];}
+        if (locations[i][3] == 'undefined'){ email ='';} else { email = locations[i][3];}
+        if (locations[i][4] == 'undefined'){ web ='';} else { web = locations[i][4];}
+        markericon ='/assets/images/pin-sm.png';
         marker = new google.maps.Marker({
             icon: markericon,
             position: new google.maps.LatLng(locations[i][5], locations[i][6]),
             map: map,
             title: locations[i][0],
-            desc: description,
+            // desc: description,
+            address: address,
             tel: telephone,
             email: email,
             web: web
         });
-        link = '';    
+
+        google.maps.event.addListener(marker, 'click', function(){
+          var partnerDetails = '<h4>'+this.title+'</h4>'+
+            '<p>'+this.address+'</p><br>'+
+            '<p><a href="'+this.web+'" target="_blank">'+this.web+'</a></p>'+
+            '<p>'+this.email+'</p>'+
+            '<p>'+this.tel+'</p>';
+          infowindow.setContent(partnerDetails);
+          infowindow.open(map, this);
+        }); 
       }
+
 
     }
 
