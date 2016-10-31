098a58f65c66b378f7ba6a7d0d4907fdb7d253a7
Nate Beaty <nate@firebellydesign.com>
2016-10-31 17:02:01 -0500

    add legal page scss, use $images-dir variable to swap out to ASP path when necessary

--- assets/gulpfile.js
+++ assets/gulpfile.js
@@ -34,7 +34,7 @@ gulp.task('browser-sync', function() {
 // Sass compilation and output
 gulp.task('styles', function() {
   return gulp.src(['sass/main.scss'])
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass({outputStyle: 'extended'}).on('error', sass.logError))
     .pipe(autoprefixer())
     .pipe(gulp.dest('css'))
     .pipe(rename({suffix: '.min'}))
